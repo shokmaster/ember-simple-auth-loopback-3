38c1fb90f65ba40c97a9a4cb045f1985df9adde4
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2019-02-24 19:17:31 +0100

    test(authenticator): add tests

--- tests/unit/authenticators/loopback-test.js
+++ tests/unit/authenticators/loopback-test.js
@@ -1,55 +1,129 @@
 import $ from 'jquery';
 import Loopback from 'ember-simple-auth-loopback-3/authenticators/loopback';
 import sinon from 'sinon';
-import { module } from 'qunit';
+import { module, test } from 'qunit';
 import { run } from '@ember/runloop';
-import { test } from 'ember-qunit';
+import { setupTest } from 'ember-qunit';
 
 let subject;
 const email = 'foo@bar.com';
 const password = 'foobar';
 const url = '/User/login';
 
-module('Loopback Authenticator', {
-	beforeEach: () => {
-		subject = Loopback.create();
-		sinon.spy($, 'ajax');
-	},
-	afterEach: () => {
-		$.ajax.restore();
+const fakeMakeRequestResolve = {
+	makeRequest(loginEndpoint, identificationDataReceived) {
+		return Promise.resolve(identificationDataReceived);
 	}
-});
+};
+const fakeMakeRequestReject = {
+	makeRequest() {
+		return Promise.reject({ responseJSON: '{}' });
+	}
+};
+const fakeMakeRequestReject2 = {
+	makeRequest() {
+		return Promise.reject({ responseText: '{}' });
+	}
+};
 
-test('#restore resolves with the correct data', (assert) => {
-	const data = { id: 'foo' };
+module('Unit | Authenticator | loopback', (hooks) => {
+	setupTest(hooks);
 
-	run(() => {
+	hooks.beforeEach(() => {
+		subject = Loopback.create();
+
+		sinon.spy($, 'ajax');
+	});
+
+	hooks.afterEach(() => {
+		$.ajax.restore();
+	});
+
+	test('#restore resolves with the correct data', (assert) => {
+		const data = { id: 'foo' };
+
 		subject.restore(data).then((content) => {
 			assert.deepEqual(content, data);
 		});
 	});
-});
 
-test('#invalidate returns a resolving promise', (assert) => {
-	subject.invalidate().then(() => {
-		assert.ok(true);
+	test('#restore rejects with empty data', (assert) => {
+		const data = {};
+
+		subject.restore(data).catch(() => {
+			assert.ok(true, 'promise rejected');
+		});
 	});
-});
 
-test('#authenticate sends an AJAX request to the login endpoint', (assert) => {
-	subject.authenticate(email, password);
+	test('#invalidate returns a resolving promise when a logoutEndpoint is not setted', (assert) => {
+		subject.invalidate().then(() => {
+			assert.ok(true);
+		});
+	});
 
-	run(() => {
-		const args = $.ajax.getCall(0).args[0];
+	test('#invalidate returns a resolving promise when a logoutEndpoint is setted', (assert) => {
+		subject.set('logoutEndpoint', '/logout');
 
-		delete args.beforeSend;
+		subject.invalidate().then(() => {
+			assert.ok(true);
+		});
+	});
 
-		assert.deepEqual(args, {
-			contentType: 'application/json',
-			data: `{"email":"${email}","password":"${password}"}`,
-			dataType: 'json',
-			type: 'POST',
-			url
+	test('#invalidate returns a resolving promise when logoutEndpoint and token are setted', (assert) => {
+		subject.set('logoutEndpoint', '/logout');
+
+		subject.invalidate({ id: 'fooBarToken' }).then(() => {
+			assert.ok(true);
+		});
+	});
+
+	test('#authenticate calls `makeRequest` with email/password', async(assert) => {
+		const authenticator = Loopback.extend(fakeMakeRequestResolve).create();
+
+		const result = await authenticator.authenticate(email, password);
+
+		assert.deepEqual(result, { email, password }, 'makes the request with email and password');
+	});
+
+	test('#authenticate rejects the error of `responseJSON` if authentication fails', (assert) => {
+		assert.expect(1);
+
+		const done = assert.async();
+		const authenticator = Loopback.extend(fakeMakeRequestReject).create();
+
+		authenticator.authenticate(email, password).catch((error) => {
+			assert.equal(error, '{}', 'rejects the content of `responseJSON` field');
+			done();
+		});
+	});
+
+	test('#authenticate rejects the error of `responseText` if authentication fails', (assert) => {
+		assert.expect(1);
+
+		const done = assert.async();
+		const authenticator = Loopback.extend(fakeMakeRequestReject2).create();
+
+		authenticator.authenticate(email, password).catch((error) => {
+			assert.equal(error, '{}', 'rejects the content of `responseText` field');
+			done();
+		});
+	});
+
+	test('#authenticate sends an AJAX request to the login endpoint', (assert) => {
+		subject.authenticate(email, password);
+
+		run(() => {
+			const args = $.ajax.getCall(0).args[0];
+
+			delete args.beforeSend;
+
+			assert.deepEqual(args, {
+				contentType: 'application/json',
+				data: `{"email":"${email}","password":"${password}"}`,
+				dataType: 'json',
+				type: 'POST',
+				url
+			});
 		});
 	});
 });
